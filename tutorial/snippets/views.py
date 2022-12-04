ed75f6e1ad55b26d7572d46c0d7ba0460deb31fa
owen.ki <owen.ki.dev@gmail.com>
2022-12-04 22:08:20 +0900

    Tutorial 4 - Object level permissions

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,14 +1,15 @@
+from django.contrib.auth.models import User
+from rest_framework import generics
+from rest_framework import permissions
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
-from rest_framework import generics
-from django.contrib.auth.models import User
-from rest_framework import permissions
+from snippets.permissions import IsOwnerOrReadOnly
 
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
